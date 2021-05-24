6a87a8c319063f0f6c9036791f5e26b3cd8b48ce
Marcos Fabricio Sizanosky <fabricio_sizanosky@hotmail.com>
2021-05-24 01:39:45 -0300

    Atualizadas as funções do projeto

---
+++ dados.py
@@ -0,0 +1,33 @@
+# "Banco de dados"
+alunos = {
+    "marcosfs": {
+        "nome": "Marcos Fabricio Sizanosky",
+        "trabalhos": [90, 95, 85, 100],
+        "provas": [90, 90],
+        "laboratorio": [90, 85.2]
+    },
+    "jeffersons": {
+        "nome": "Jefferson Santos",
+        "trabalhos": [70, 60, 60, 50],
+        "provas": [90, 60],
+        "laboratorio": [70, 55.2]
+    },
+    "mariacbs": {
+        "nome": "maria clara bruske sizanosky",
+        "trabalhos": [77, 82, 90, 39],
+        "provas": [80, 90],
+        "laboratorio": [70, 44.53]
+    },
+    "lucasbs": {
+        "nome": "lucas bruske sizanosky",
+        "trabalhos": [67, 95, 77, 95],
+        "provas": [80, 88.45],
+        "laboratorio": [100, 44.56]
+    },
+    "priscilabs": {
+        "nome": "priscila bruske da silva",
+        "trabalhos": [95, 89, 90, 100],
+        "provas": [90, 90],
+        "laboratorio": [70, 44.6]
+    },
+}
